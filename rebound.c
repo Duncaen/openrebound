a45ce7bee9276db7f7f05d03498daabe5da879c1
tedu <tedu>
2015-10-17 00:38:57 +0000

    don't need fcntl for non blocking socket, just ask for it upfront

--- rebound.c
+++ rebound.c
@@ -24,7 +24,6 @@
 #include <sys/signal.h>
 #include <sys/wait.h>
 
-#include <fcntl.h>
 #include <signal.h>
 #include <syslog.h>
 #include <stdlib.h>
@@ -308,10 +307,8 @@ newtcprequest(int ld, struct sockaddr *remoteaddr)
 	if (req->client == -1)
 		goto fail;
 
-	req->s = socket(remoteaddr->sa_family, SOCK_STREAM, 0);
+	req->s = socket(remoteaddr->sa_family, SOCK_STREAM | SOCK_NONBLOCK, 0);
 	if (req->s == -1)
-		goto fail;
-	if (fcntl(req->s, F_SETFL, O_NONBLOCK) == -1)
 		goto fail;
 	req->phase = 1;
 	if (connect(req->s, remoteaddr, remoteaddr->sa_len) == -1) {
